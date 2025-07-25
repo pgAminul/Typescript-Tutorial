b4d7fad490d030ed8b3adb5ab6f55d66dfd023fb
pgAminul <businessbyaminul@gmail.com>
2025-07-26 00:05:30 +0800

    setup typescript

---
+++ allJavascript/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let myName = "Aminul Islam";
+let age = 22;
+let married = false;
+console.log(myName, age, married);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let myName: string = "Aminul Islam";
+let age: number = 22;
+let married: boolean = false;
+console.log(myName, age, married);
